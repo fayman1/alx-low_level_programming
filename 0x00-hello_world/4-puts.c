48ff3bf3d0a432d572c2cab127fbbac69619845a
Faith Muigai <fmuigai269@gmail.com>
2023-06-15 18:54:20 -0700

    4--puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: prints out to stdout "Programming is like building a multilingual puzzle"
+ * Description: prints to stdoout
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
