3ee4515777e278ad8a083e26e151da81943060bf
Faith Muigai <fmuigai269@gmail.com>
2023-06-17 13:22:06 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,21 +10,18 @@
 
 int main(void)
 {
-    int x;
+	int x;
 
-    for (x = '0'; x <= '9'; x++)
-    {
-        putchar(x);
-
-        if (x != '9')
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
