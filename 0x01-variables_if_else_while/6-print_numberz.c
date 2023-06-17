3b70ffd10e45f2bc0d070fbe1c531ae7cafc3b19
Faith Muigai <fmuigai269@gmail.com>
2023-06-17 09:42:25 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print single digit number followed by single line
+ * Return: 0 (succes)
+*/
+
+int main(void)
+{
+	int val_digit;
+
+	for (val_digit = 0; val_digit < 10; val_digit++)
+
+	{
+		putchar((val_digit % 10) + '0');
+	}
+	printf("\n");
+	return (0);
+}
