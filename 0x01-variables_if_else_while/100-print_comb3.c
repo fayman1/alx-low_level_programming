c8938eec98be083cea376fb2019098d882de60bf
Faith Muigai <fmuigai269@gmail.com>
2023-06-17 14:06:30 -0700

    inventing is a combination of brain

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ int main(void)
 	int num_val;
 	int num_s;
 
-	for (num_val = 0; num_val < 10; num_val++)
-		for (num_s = num_val + 1; num_s < 9; num_s++)
+	for (num_val = 0; num_val < 9; num_val++)
+		for (num_s = num_val + 1; num_s < 10; num_s++)
 		putchar((num_val % 10) + '0');
 	putchar((num_s % 10) + '0');
 	if (num_val == 8 && num_s == 9)
