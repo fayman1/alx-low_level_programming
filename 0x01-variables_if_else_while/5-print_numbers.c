03046c118b3f9c8c7abbabba637a47c8fa2721a1
Faith Muigai <fmuigai269@gmail.com>
2023-06-17 09:24:18 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit base 10 number
+ * Return: 0
+*/
+
+int main(void)
+{
+	int val_digit;
+
+	for (val_digit = 0; val_digit < 10; val_digit++)
+
+	{
+		printf("%d", val_digit);
+	}
+	printf("\n");
+	return (0);
+}
