b1a7b5788fa9f8c898858cb1ed32c3d8afb240f0
Faith Muigai <fmuigai269@gmail.com>
2023-06-17 11:59:31 -0700

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all single digit number combination
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int val;
+	{
+		for (val = 0; val <= 9; val++)
+			putchar((val % 10) + '0');
+		if (val == 9)
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
