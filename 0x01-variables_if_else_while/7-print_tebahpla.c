933bdadf42496884444304a92ef53e57b8e9cfcd
Faith Muigai <fmuigai269@gmail.com>
2023-06-17 10:20:41 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print lowercase in reverse
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char reverse;
+
+	for (reverse = 'z'; reverse > 'a'; reverse--)
+		putchar(reverse);
+	putchar('\n');
+	return (0);
+}
