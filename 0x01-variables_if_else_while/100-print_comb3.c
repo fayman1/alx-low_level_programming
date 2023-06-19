386fd159310034fe0a2f966cda28baae4b586885
Faith Muigai <fmuigai269@gmail.com>
2023-06-19 12:35:15 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,16 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 1; a <= 10; a++)
+	for (a = '1'; a <= '10'; a++)
 	{
-		for (b = a + 1; b <= 10; b++)
+		for (b = a + '1'; b <= '10'; b++)
 		{
 			if (b != a)
 			{
-				putchar(a, '0');
-				putchar(b, '0');
+				putchar(a);
+				putchar(b);
 
-				if (a == 8 && b == 9)
+				if (a == '8' && b == '9')
 					continue;
 
 				putchar(',');
