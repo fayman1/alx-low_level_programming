fefbd469135357c5bbff17238c6aca3518d88789
Faith Muigai <fmuigai269@gmail.com>
2023-06-20 16:42:01 -0700

    ever liber abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print the sum of the even-valued terms
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int o = 1, p = 2, val = 0;
+
+	if (p < 4000000)
+	{
+		while (p % 2 == 0)
+			val += p;
+		o = a;
+		a = p;
+		p += o;
+	}
+	printf("%d\n", val);
+	return (0);
+}
