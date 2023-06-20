d0fde57ce5b3a44c308b437d53f2e32ddb20d60e
Faith Muigai <fmuigai269@gmail.com>
2023-06-20 07:21:59 -0700

    created 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print  in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 'a'; m <= 'z'; m++)
+
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
