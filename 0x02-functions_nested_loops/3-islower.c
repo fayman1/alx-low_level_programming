ec04f0788a991f6458b4fe1ce6b74e8a037439d2
Faith Muigai <fmuigai269@gmail.com>
2023-06-20 09:12:58 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower -  function that checks for lowercase
+ * @c: Thee character to be checked
+ * Return: 1 otherwise 0
+ */
+
+int _islower(int c)
+{
+	return (c <= 97 && c <= 122);
+}
