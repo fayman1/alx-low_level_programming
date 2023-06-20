6a63b3add5c7cf1a38f125bb31678482a4959afe
Faith Muigai <fmuigai269@gmail.com>
2023-06-20 13:31:22 -0700

    the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all natural numbers from n to 98
+ * @n: character to check
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("98\n");
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("98\n");
+	}
+	else if (n == 98)
+		printf("98\n");
+}
