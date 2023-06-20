97bfe0ff168bd99b989cec35c1b02defaa4b8a71
Faith Muigai <fmuigai269@gmail.com>
2023-06-20 06:32:27 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int add(int, int);
+void print_to_98(int n);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+int _islower(int c);
+int _isalpha(int c);
+void print_times_table(int n);
+int _putchar(char c);
+int _abs(int);
+int print_last_digit(int);
+int print_sign(int n);
+
+#endif
