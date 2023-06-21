fa873e316330a236b80e0aa4591fdf9cfb6e1f09
Faith Muigai <fmuigai269@gmail.com>
2023-06-20 17:34:16 -0700

    last

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,18 +9,34 @@
 
 int main(void)
 {
-	int s = 1;
-	int t = 1;
-	int q = 1;
-	int r = 0;
+	unsigned long int a, b, c, b1, b2, c1, c2;
 
-	while (q < 99)
+	b = 1;
+	c = 2;
+
+	printf("%lu", b);
+
+	for (a = 1; a < 91; a++)
 	{
-		printf("%d, ", s);
-		r = s;
-		s = s + t;
-		t = r;
-		q++;
+		printf(", %lu", c);
+		c = c + b;
+		b = c - b;
 	}
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	c1 = c / 1000000000;
+	c2 = c % 1000000000;
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", c1 + (c2 / 1000000000));
+		printf("%lu", c2 % 1000000000);
+		c1 = c1 + b1;
+		b1 = c1 - b1;
+		c2 = c2 + b2;
+		b2 = c2 - b2;
+	}
+	printf("\n");
+
 	return (0);
 }
