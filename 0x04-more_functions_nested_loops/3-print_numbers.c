796e0c81a7449492f3baa8a78d411c9faf35b4fe
Faith Muigai <fmuigai269@gmail.com>
2023-06-22 04:48:03 -0700

    nnumbers speaks for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
