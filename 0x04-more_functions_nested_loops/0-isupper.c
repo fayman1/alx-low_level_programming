7346d0cd49feb06aa9d811709274e43f7c1465bb
Faith Muigai <fmuigai269@gmail.com>
2023-06-21 22:46:03 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercases
+ * @c: character to be checked
+ * Return: 1 otherwise 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
