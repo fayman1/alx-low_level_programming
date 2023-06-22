6f1802c86ab3ea2b86834e1866bc31aefc49271f
Faith Muigai <fmuigai269@gmail.com>
2023-06-22 04:58:35 -0700

    believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers from 0 - 9
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x !=  2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
