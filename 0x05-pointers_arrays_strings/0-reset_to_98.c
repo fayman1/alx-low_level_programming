4690235d9666d1531dc52b8f85ac0abdbea09946
Faith Muigai <fmuigai269@gmail.com>
2023-06-25 23:48:28 -0700

    98 Battery ST

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * as parameter and updates the value it points to 98
+ * @n: int to be checked
+ * Return: 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
