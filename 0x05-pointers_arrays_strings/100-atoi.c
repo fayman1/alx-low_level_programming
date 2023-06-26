abb549e1a9d392a9a9f0e59c1cada6c86f3c4b9b
Faith Muigai <fmuigai269@gmail.com>
2023-06-26 15:24:58 -0700

    sacrifice the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string input
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int num = 0;
+	int val = 1;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			val = -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+		s++;
+	}
+	return (num * val);
+}
+
